732b0d302fe019e4a70e206416b10eedf89eaf49
KimInKeun <49625207+KimInKeun@users.noreply.github.com>
2019-04-17 15:25:51 +0900

    Review 의견 반영

--- goods.h
+++ goods.h
@@ -1,12 +1,12 @@
-#ifndef GOODS_H_
-#define GOODS_H_
+#ifndef CODEREVIEW_GOODS_H_
+#define CODEREVIEW_GOODS_H_
 
 // Goods class for representing goods information with index and price
 // Example:
 //    Goods test;
 //    test.SetGoodsInfo(1, 10000);
 //    std::cout << test.GetIndex() << " - " << test.GetPrice << endl;
-class Goods{
+class Goods {
  public:
   void SetGoodsInfo(int index, int price);
   int GetIndex();
@@ -18,4 +18,4 @@ class Goods{
 };
 
 
-#endif  // GOODS_H_
+#endif  // CODEREVIEW_GOODS_H_
